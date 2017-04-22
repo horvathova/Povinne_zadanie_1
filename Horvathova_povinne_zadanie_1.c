36f4705fedd5b25ddd682ef97880d606ae5d2c36
DESKTOP-QBE76N6\nikol <nikoleta638@gmail.com>
2017-04-22 18:01:26 +0200

    no message

--- Horvathova_povinne_zadanie_1.c
+++ Horvathova_povinne_zadanie_1.c
@@ -2,91 +2,94 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
-
+#define MAX 255
 
 char nahodny_polynom(float *q,char rad,char k){
 	char i,j;
-	float min_rand,max_rand,range,koeficienty[255],K[255],sucin;
-	int korene,pocet;
+	float min_rand,max_rand,range,koeficienty[MAX],K[MAX],A[MAX],B[MAX];
+	int korene,pocet_k,stupen,pom;
 	
 	min_rand=-5.0;
 	max_rand=5.0;
-	pocet=0;
-	sucin=1.;
+	pocet_k=0;
 	range=max_rand-min_rand;
+	stupen=(int)rad;
+	pom=(int)k;
+	
+	if(pom>stupen){
+		return printf("FAILED");
+	}
+	
+	else {
+		if(rad=='0'){
+			while(k!='0'&& k!='1'){
+				return printf("FAILED");
+			}	
+		}	
+	else if(rad=='1'){
+		while(k!='1'){
+			return printf("FAILED");	
+		}
+	}
+	else if(stupen%2==0){
+		while((pom%2)!=0){
+		 	return printf("FAILED");
+		}
+		while(k=='0'){
+			return printf("FAILED");
+		}
+	}		
+	else if(stupen%2==1){
+		while((pom%2)!=1){
+			return printf("FAILED");
+		}
+	}
+			
 	printf("KORENE:\n");
-	for (i='0';i<k;i++){
+	for(i='0';i<k;i++){
 		K[i]=range*((((float)rand())/(float)RAND_MAX))+min_rand;
-		printf("%.1lf\n",K[i]);
-		sucin=sucin*K[i];
-		pocet++;
+		printf("%.3lf\n",K[i]);
+		pocet_k++;
+	}
+	
 	}	
 	
-	pocet++;
-	printf("%.2lf\n",sucin);
-	koeficienty['0']=1.;
-	printf("0.koeficient %.1lf\n",koeficienty['0']);
-	for (i='1';i<=k;i++){
-		koeficienty[i]=pow(-1,pocet-i);
-		printf("%c koeficient%.1lf\n",i,koeficienty[i]);	
+	A['0']=1.;
+	A['1']=K['0'];
+	B['0']=0.;
+	
+	printf("\n");
+	printf("%.1lf ",A['0']);
+	printf("%.1lf\n",A['1']);
+	
+	printf("VYMENA\n");
+	printf("%.3lf ",B['0']);
+	for(i='1';i<k;i++){
+		B[i]=A[i-1];
+		printf("%.3lf ",B[i]);
 	}	
+	printf("\n");
+	for(i='0';i<k;i++){
+		B[i]=K['1']*B[i];
+		printf("%.3lf ",B[i]);
+	}
+	printf("\nVysledok\n");
+	for(i='0';i<k;i++){
+		koeficienty[i]=A[i]+B[i];
+		printf("%.3lf ",koeficienty[i]);
+	}
 }
 
 
 main(){
-	char polynom,stupen,koren;
+	char stupen,koren,polynom;
 	float *smernik;
-	int rad,pom;
 
 	srand(time(NULL));
 	printf("Zadaj najvacsi stupen polynoumu:");
 	scanf(" %c",&stupen);
 	printf("Zadaj pocet realnych korenov polynomu:");
 	scanf(" %c",&koren);
-	rad=(int)stupen;
-	pom=(int)koren;
-	printf("korene %d\n",pom);
-	printf("stupen %d\n",rad);
 	
-	printf("PO DELENI %d\n",pom%2);
-
-	if(stupen=='0'){
-		while(koren!='0'&& koren!='1'){
-				printf("Zadaj znovu:\n");
-				scanf(" %c",&koren);	
-		}	
-	}	
-	else if(stupen=='1'){
-		while(koren!='1'){
-			printf("Zadaj pocet realnych korenov polynomu:\n");
-			scanf(" %c",&koren);	
-		}
-	}
-	else if(rad%2==0){
-		while((pom%2)!=0){
-		 	printf("Zadaj pocet realnych korenov polynomu:\n");
-		 	scanf(" %c",&koren);
-			pom=(int)koren;
-		}
-		while(koren>stupen){
-		 	printf("Zadaj pocet realnych korenov polynomu:\n");
-		 	scanf(" %c",&koren);
-		}
-		while(koren=='0'){
-			printf("Zadaj pocet realnych korenov polynomu:\n");
-		 	scanf(" %c",&koren);
-		}
-	}		
-	else if(rad%2==1){
-		while((pom%2)!=1){
-			printf("Zadaj pocet realnych korenov polynomu:\n");
-		 	scanf(" %c",&koren);
-			pom=(int)koren;	
-		}
-		while(koren>stupen){
-		 	printf("Zadaj pocet realnych korenov polynomu:\n");
-		 	scanf(" %c",&koren);
-		}
-	}				
 	polynom=nahodny_polynom(&smernik,stupen,koren);
 }
