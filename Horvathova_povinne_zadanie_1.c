5f134ebd146a7c2fd8ba213754498c4bfb4f835c
DESKTOP-QBE76N6\nikol <nikoleta638@gmail.com>
2017-06-21 22:55:05 +0200

    no message

--- Horvathova_povinne_zadanie_1.c
+++ Horvathova_povinne_zadanie_1.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #define FAIL '0'
 #define OK '1'
 
 char nahodny_polynom(float *q, char rad, char k){
-	float min_rand, max_rand, range, *korene, *A, *B;
+	float min_rand, max_rand, range, *korene, *A, *B, *KVADR, *POLY;
 	int i,j;
+	unsigned char pocet;
 	
 	min_rand=-5.0;
 	max_rand=5.0;
@@ -28,7 +30,7 @@ char nahodny_polynom(float *q, char rad, char k){
 		}
 		
 		else if(k==1){
-            q[0]=1.;
+            q[0]=1;
 			q[1]=range*((((float)rand())/(float)RAND_MAX))+min_rand;
         return OK; 
 		}
@@ -40,7 +42,7 @@ char nahodny_polynom(float *q, char rad, char k){
 		}	
 		
 		else if(k==0){
-			q[0]=1.;
+			q[0]=1;
 			q[rad]=range*((float)rand())/((float)RAND_MAX)+0.;
 			return OK;		
 		}
@@ -52,25 +54,27 @@ char nahodny_polynom(float *q, char rad, char k){
 		}
 		
 		if(k==1){
-			q[0]=1.;
+			q[0]=1;
 			q[1]=range*((((float)rand())/(float)RAND_MAX))+min_rand;
 			return OK;
 		}
 	}
 	
+	korene=malloc(k*sizeof(float));	
     for(i=0;i<(int)k;i++){
-    	korene=malloc(k*sizeof(float));
         korene[i]=range*((((float)rand())/(float)RAND_MAX))+min_rand;
         printf("%lf ",korene[i]);
     }
-
+	printf("\n");
+	
 	A=calloc(k,sizeof(float));
-	B=calloc(k+1,sizeof(float));	
-	A[0]=1.;
+	B=calloc(k+1,sizeof(float));
+	
+	A[0]=1;
 	A[1]=korene[0];
 	for(j=1;j<(int)k;j++){
 		for(i=0;i<(int)k+1;i++){
-			B[0]=0.;
+			B[0]=0;
 			B[i+1]=korene[j]*A[i];
 			q[i]=A[i]+B[i];
 			
@@ -78,9 +82,42 @@ char nahodny_polynom(float *q, char rad, char k){
 		
 		for(i=0;i<(int)k+1;i++){
 			A[i]=q[i];
-		}
+		}	
+	}
+
+	printf("POLYNOM:\n");
+	for(i=0;i<(int)k+1;i++){
+		printf("%.2lf ",q[i]);
 	}
 	
+	if(k<rad){
+		pocet=rad-k;
+		KVADR=calloc(pocet, sizeof(float));
+		POLY=calloc(k+1, sizeof(float));
+		
+		KVADR[0]=1;
+		KVADR[pocet]=range*((float)rand())/((float)RAND_MAX)+0.;
+		printf("KVADR:\n");
+		for(i=0;i<pocet+1;i++){
+			printf("%.2lf ",KVADR[i]);
+		}
+		
+		printf("\n");
+		
+		for(i=0;i<(int)k+1;i++){
+			POLY[i]=q[i];
+			q[i]=0;
+		}
+		
+		for(i=0;i<(int)rad;i++){
+			for(j=0;j<(int)pocet+1;j++){
+				q[i+j]+= POLY[i]*KVADR[j];
+			}	
+		}
+		
+	}
+	
+	printf("\n");
 	return OK;
 }
 
@@ -115,9 +152,10 @@ main(){
 	
 	if(polynom!='0'){
 		for(i=0;i<stupen+1;i++){
-		printf("%.2f\n", koeficienty[i]);
+		printf("x^%d %.2f\n", rad, koeficienty[i]);
 		rad--;
 		}
 	}
+	
 	//free(koeficienty);
 }
