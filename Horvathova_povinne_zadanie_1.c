2aaa475772338aa092dc8365078a4674a1258e20
horvathova <nikoleta638@gmail.com>
2018-06-12 11:12:20 +0200

    Update Horvathova_povinne_zadanie_1.c
    Pridané funkcie free()

--- Horvathova_povinne_zadanie_1.c
+++ Horvathova_povinne_zadanie_1.c
@@ -17,78 +17,61 @@ double* generuj_korene_vsucine(char k) {
 	for (i = 0; i<(int)k; i++) {
 		korene[i] = range*((double)rand() / (double)RAND_MAX) + min_rand;
 	}
+
 	return korene;
-}
-
-double* generuj_korenovy_polynom(char k, double *korene) {
-	int i, j;
-	double *A, *B, *POLY;
-
-	A = calloc(k+1, sizeof(double));
-	B = calloc(k + 1, sizeof(double));
-	POLY = calloc(k + 1, sizeof(double));
-
-	A[0] = 1;
-	A[1] = korene[0];
-
-	if (k == 1) {
-		return A;
-	}
-
-	else {
-		for (j = 1; j<(int)k; j++) {
-			for (i = 0; i<(int)k + 1; i++) {
-				B[0] = 0;
-				B[i + 1] = korene[j] * A[i];
-				POLY[i] = A[i] + B[i];
-			}
-
-			for (i = 0; i<(int)k + 1; i++) {
-				A[i] = POLY[i];
-			}
-		}
-	}
-
-    // TODO: dynamicky allokovane bloky pamati treba uvolnovat
-
-	/*free(A);
-	free(B);*/
-	return POLY;
 }
 
 double* vynasob_polynomy(double *prvy, double *druhy, char const stupen1, char const stupen2) {
 	int i, j;
-	double *final = calloc((stupen1 + stupen2+1), sizeof(double));
+	double *final = calloc((stupen1 + stupen2 + 1), sizeof(double));
 
 	for (i = 0; i <= (int)stupen1; i++) {
 		for (j = 0; j <= (int)stupen2; j++) {
 			final[i + j] += prvy[i] * druhy[j];
 		}
 	}
+
 	return final;
+}
+
+double* generuj_korenovy_polynom(char k, double *korene) {
+	double *A = malloc(k * sizeof(double));
+	double B[2];
+	double *vysledny = malloc((k + 1) * sizeof(double));
+	int i, j;
+
+	A[0] = 1.;
+	A[1] = korene[0];
+
+	if (k == 1) {
+		return A;
+	}
+	else {
+		B[0] = 1.;
+
+		for (i = 1; i < k; i++) {
+			B[1] = korene[i];
+			vysledny = vynasob_polynomy(A, B, i, 1);
+
+			if (i != k) {
+				A = vysledny;
+			}
+			else {
+				free(A);
+			}
+		}
+	}
+	
+	return vysledny;
 }
 
 double* generuj_komplex_polynom() {
 	double a, b, c;
-	int j;
-	const double range = max_rand - min_rand;
 	double *kvadraticky = malloc(3 * sizeof(double));
 
-	a = range*((double)rand() / (double)RAND_MAX) + min_rand;                   // -3  az 3
-	if (a < 0) {
-		c = -1 * (range + 5)*((double)rand() / (double)RAND_MAX) + min_rand;    // -14 az -3
-	}
-	else {
-		c = (range + 5)*((double)rand() / (double)RAND_MAX) + 2*max_rand;       //  6  az 17
-	}
-
-	b = (range - 4)*((double)rand() / (double)RAND_MAX) + min_rand;             // -3  az -1
-
-    // TODO: zbavte sa tohto cyklu
-
-	while (b* b > 4 * a * c) {
-		b = (range - 4)*((double)rand() / (double)RAND_MAX) + min_rand;
-	}
+	a = (max_rand*((double)rand() / (double)RAND_MAX)) + max_rand;		//  3 az 6
+	c = (max_rand*((double)rand() / (double)RAND_MAX)) + max_rand;		//  3 az 6
+	b = (max_rand + 1)*((double)rand() / (double)RAND_MAX);             // 0 az 4
 
 	kvadraticky[0] = a;
 	kvadraticky[1] = b;
@@ -101,57 +84,45 @@ double* generuj_zvysny_polynom(const char rad, const char k) {
 	int pocet = rad - k;
 	int kvadr = pocet / 2;
 	int j = 4;
+	int i;
 	bool prva_iter = true;
-
-    // TODO: tu allokujete niektore veci, ktorych adresu vzapate zabudate
-
-	double *pom = malloc(pocet * sizeof(double));
+	double *pom;
 	double *pom2 = malloc(pocet * sizeof(double));
 	double *kvadraticky = malloc(3 * sizeof(double));
-	double *zvysny = malloc(pocet * sizeof(double));	
-	
-	if(pocet==2){
+	double *zvysny = malloc(pocet * sizeof(double));
+
+	if (pocet == 2) {
 		return zvysny = generuj_komplex_polynom();
 	}
 
-	for (int i = 0; i < kvadr / 2; i++) {
+	kvadraticky = generuj_komplex_polynom();
+	pom2 = vynasob_polynomy(kvadraticky, kvadraticky, 2, 2);
+
+	if (kvadr / 2 != 1) {
+		pom = pom2;
+	}
+	else {			//ak je potrebny zvysny polynom stupna 4
+		zvysny = pom2;
+		pom = zvysny;
+	}
+
+	for (i = 1; i < kvadr / 2; i++) {
 		kvadraticky = generuj_komplex_polynom();
 		pom2 = vynasob_polynomy(kvadraticky, kvadraticky, 2, 2);
-
-		//toto zbehne len prvykrat, pretoze sa nic nenasobi, len sa nastavi zaciatocny polynom 
-		if (prva_iter) {
-			if(kvadr/2 !=1){
-				pom = pom2;
-			}
-
-			//ak je potrebny zvysny polynom stupna 4
-			else {
-				zvysny = pom2;
-			}
-
-			prva_iter = false;
-		}
-
-		else {
-			zvysny = vynasob_polynomy(pom2, pom, 4, j);
-			pom = zvysny;
-			j += 4;
-		}
+		zvysny = vynasob_polynomy(pom2, pom, 4, j);
+		pom = zvysny;
+		j += 4;
 	}
 
 	//ak potrebujeme neparny pocet kvadratickych polynomov
 	if (kvadr % 2 != 0) {
 		kvadraticky = generuj_komplex_polynom();
 		pom = zvysny;
-
-		zvysny = vynasob_polynomy(kvadraticky, pom, 2, pocet -2);
+		zvysny = vynasob_polynomy(kvadraticky, pom, 2, pocet - 2);
 	}
 
-    // TODO: dynamicky allokovane veci je NUTNE uvolnovat
-
-	/*free(kvadraticky);
-	free(pom);
-	free(pom2);*/
+	free(kvadraticky);
+	free(pom2);
 
 	return zvysny;
 }
@@ -171,7 +142,6 @@ char nahodny_polynom(double *q, char rad, char k) {
 		if ((int)k % 2 == 1) {
 			return FAIL;
 		}
-
 		else if (k == 0) {
 			q[0] = 1.;
 			q[rad] = range*((double)rand()) / ((double)RAND_MAX) + 0.;
@@ -187,14 +157,16 @@ char nahodny_polynom(double *q, char rad, char k) {
 	}
 
 	korene = generuj_korene_vsucine(k);
+	printf("Vygenerovane korene: ");
 
-    printf("Vygenerovane korene: ");
-    for(i=0; i<k; i++)
-        printf("%f ", korene[i]);
-    printf("\n");
+	for (i = 0; i < k; i++) {
+		printf("%f ", korene[i]);
+	}
 
+	printf("\n");
 	korenovy_polynom = generuj_korenovy_polynom(k, korene);
 	printf("\nVYGENEROVANY POLYNOM %d STUPNA\n", k);
+
 	for (i = 0; i <= (int)k; i++) {
 		printf("%.3lf ", korenovy_polynom[i]);
 	}
@@ -202,25 +174,24 @@ char nahodny_polynom(double *q, char rad, char k) {
 	if (k < rad) {
 		zvysny_polynom = generuj_zvysny_polynom(rad, k);
 		printf("\nZVYSNY POLYNOM %d STUPNA\n", rad - k);
+
 		for (i = 0; i <= (int)rad - k; i++) {
 			printf("%.3lf ", zvysny_polynom[i]);
 		}
+
 		pocet = (int)rad;
 		printf("\nVYNASOBENE POLYNOMY STUPNA %d a %d\n", k, rad - k);
 		q = vynasob_polynomy(korenovy_polynom, zvysny_polynom, k, rad - k);
+
 		for (i = 0; i <= (int)rad; i++) {
 			printf("%.3lfx^%d ", q[i], pocet);
 			pocet--;
 		}
 
-
-        // TODO: segfault, zistit kde a preco
-
-        /*
 		free(korene);
 		free(korenovy_polynom);
 		free(zvysny_polynom);
-        */
+		
 		return OK;
 	}
 
@@ -235,6 +206,7 @@ int main() {
 	srand(time(NULL));
 	printf("Zadaj najvacsi stupen polynoumu:");
 	scanf("%d", &stupen);
+
 	while (stupen<0 || stupen>255) {
 		printf("Znovu zadaj najvacsi stupen polynoumu:");
 		scanf("%d", &stupen);
@@ -258,5 +230,5 @@ int main() {
 	printf("\nhodnota: %c\n", polynom);
 	getchar();
 	getchar();
-	//free(koeficienty);
+	free(koeficienty);
 }
